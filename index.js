3f1f53e03f3cabb6edbcf458174e6d7f23a0f638
Baklay Petro <pustota1988@gmail.com>
2023-10-05 14:38:36 +0300

    deploy

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors'); 
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors())
+
+let activeSessions = 0;
+
+io.on('connection', (socket) => {
+  activeSessions++;
+  io.emit('activeSessions', activeSessions);
+
+  socket.on('disconnect', () => {
+    activeSessions--;
+    io.emit('activeSessions', activeSessions);
+  });
+});
+
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
